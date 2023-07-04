f82bb2a65961ab1435ba5c8e93809073cc051991
Dan Armand Stancu <stancudanny@gmail.com>
2023-07-04 17:41:10 +0300

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,10 @@
+class Solution:
+    def is_rotation(self, A:list, B:list):
+        for i in A:
+            if len(B) != len (A)
+                return False
+            else:
+                for i in A:
+                    if i in B:
+                        
+        
